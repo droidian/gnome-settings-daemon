cb804d66e12d76fbc18fda54a4099207417e2129
Eugenio Paolantonio (g7) <me@medesimo.eu>
2021-05-13 00:45:23 +0200

    [power] Do not enable panel if 'disable-display-wake' is true
    Mobile devices sleep most of the time and may wakeup from sleep
    without user input to execute background tasks.
    
    In these cases, we shouldn't really enable the display. This
    setting, introduced in the schema supplied by the previous commit,
    will force the display to stay shut on wakeups.
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- plugins/power/gsd-power-manager.c
+++ plugins/power/gsd-power-manager.c
@@ -2600,6 +2600,10 @@ engine_session_properties_changed_cb (GDBusProxy      *session,
                                       GsdPowerManager *manager)
 {
         GVariant *v;
+        gboolean display_wake_disabled;
+
+        display_wake_disabled = g_settings_get_boolean (manager->settings_droidian_power,
+                                                        "disable-display-wake");
 
         v = g_variant_lookup_value (changed, "SessionIsActive", G_VARIANT_TYPE_BOOLEAN);
         if (v) {
@@ -2610,7 +2614,7 @@ engine_session_properties_changed_cb (GDBusProxy      *session,
                 manager->session_is_active = active;
                 /* when doing the fast-user-switch into a new account,
                  * ensure the new account is undimmed and with the backlight on */
-                if (active) {
+                if (active && !display_wake_disabled) {
                         idle_set_mode (manager, GSD_POWER_IDLE_MODE_NORMAL);
                         iio_proxy_claim_light (manager, TRUE);
                 } else {
@@ -2804,7 +2808,8 @@ static void
 handle_resume_actions (GsdPowerManager *manager)
 {
         /* ensure we turn the panel back on after resume */
-        backlight_enable (manager);
+        if (!g_settings_get_boolean (manager->settings_droidian_power, "disable-display-wake"))
+            backlight_enable (manager);
 
         /* set up the delay again */
         inhibit_suspend (manager);
